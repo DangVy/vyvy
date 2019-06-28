18f54e6641a07de162bd3165d6d03e04e8858805
DangVy <vydang898@gmail.com>
2019-06-28 02:34:56 -0700

    commit

--- database/migrations/2019_06_28_092410_create_cusc_chude_sanpham_table.php
+++ database/migrations/2019_06_28_092410_create_cusc_chude_sanpham_table.php
@@ -14,9 +14,15 @@ class CreateCuscChudeSanphamTable extends Migration
     public function up()
     {
         Schema::create('cusc_chude_sanpham', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->timestamps();
+            $table->engine = 'InnoDB';
+            $table->unsignedBigInteger('sp_ma')->comment('Sản phẩm # sp_ma # sp_ten # Mã sản phẩm');
+            $table->unsignedTinyInteger('cd_ma')->comment('Chủ để # cd_ma # cd_ten # Mã chủ đề');
+            
+            $table->primary(['sp_ma', 'cd_ma']);
+            $table->foreign('cd_ma')->references('cd_ma')->on('cusc_chude')->onDelete('CASCADE')->onUpdate('CASCADE');
+            $table->foreign('sp_ma')->references('sp_ma')->on('cusc_sanpham')->onDelete('CASCADE')->onUpdate('CASCADE');
         });
+        DB::statement("ALTER TABLE `cusc_chude_sanpham` comment 'Chủ đề sản phẩm # Sản phầm thuộc các chủ đề'");
     }
 
     /**
