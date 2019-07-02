68b2b4a64fdbb6504711d3505c5f871530da0999
DangVy <vydang898@gmail.com>
2019-07-01 21:24:44 -0700

    commit

---
+++ app/Http/Controllers/BackendController.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+class BackendController extends Controller
+{
+    public function dashboard() {
+        return view('backend.dashboard');
+    }
+}
